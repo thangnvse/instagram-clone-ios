861e86353f960263a874366de97bb26dccebfc79
thangnvse <thangnvse04385@fpt.edu.vn>
2018-11-21 16:57:35 +0700

    Init project

---
+++ InstagramClone/SignUpViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SignUpViewController.swift
+//  InstagramClone
+//
+//  Created by toanpq1.dtl on 11/21/18.
+//  Copyright © 2018 toanpq1.dtl. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    @IBAction func dismiss_onClick(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ InstagramClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by toanpq1.dtl on 11/21/18.
+//  Copyright © 2018 toanpq1.dtl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
