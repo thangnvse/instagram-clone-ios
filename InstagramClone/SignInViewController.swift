a2dbac19475f096bfdc50c8f7226d92d9e96184c
thangnvse <thangnvse04385@fpt.edu.vn>
2018-11-26 17:18:27 +0700

    Add firebase

--- InstagramClone/SignInViewController.swift
+++ InstagramClone/SignInViewController.swift
@@ -7,16 +7,32 @@
 //
 
 import UIKit
-
+import Firebase
 class SignInViewController: UIViewController {
-
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        emailTextField.backgroundColor = UIColor.clear
+        emailTextField.tintColor = UIColor.white
+        emailTextField.textColor = UIColor.white
+        emailTextField.attributedPlaceholder = NSAttributedString(string: emailTextField.placeholder!, attributes: [NSAttributedString.Key.foregroundColor: UIColor(white: 1.0, alpha: 0.6)])
+        let bottomLayerEmail = CALayer()
+        bottomLayerEmail.frame = CGRect(x: 0, y: 29, width: 1000, height: 0.6)
+        bottomLayerEmail.backgroundColor = UIColor.white.cgColor
+        emailTextField.layer.addSublayer(bottomLayerEmail)
+        
+        passwordTextField.backgroundColor = UIColor.clear
+        passwordTextField.tintColor = UIColor.white
+        passwordTextField.textColor = UIColor.white
+        passwordTextField.attributedPlaceholder = NSAttributedString(string: passwordTextField.placeholder!, attributes: [NSAttributedString.Key.foregroundColor: UIColor(white: 1.0, alpha: 0.6)])
+        let bottomLayerPass = CALayer()
+        bottomLayerPass.frame = CGRect(x: 0, y: 29, width: 1000, height: 0.6)
+        bottomLayerPass.backgroundColor = UIColor.white.cgColor
+        passwordTextField.layer.addSublayer(bottomLayerPass)
     }
     
-
     /*
     // MARK: - Navigation
 
--- InstagramClone/SignUpViewController.swift
+++ InstagramClone/SignUpViewController.swift
@@ -7,19 +7,52 @@
 //
 
 import UIKit
-
+import FirebaseAuth
 class SignUpViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passTextField: UITextField!
     @IBAction func dismiss_onClick(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        emailTextField.backgroundColor = UIColor.clear
+        emailTextField.tintColor = UIColor.white
+        emailTextField.textColor = UIColor.white
+        emailTextField.attributedPlaceholder = NSAttributedString(string: emailTextField.placeholder!, attributes: [NSAttributedString.Key.foregroundColor: UIColor(white: 1.0, alpha: 0.6)])
+        let bottomLayerEmail = CALayer()
+        bottomLayerEmail.frame = CGRect(x: 0, y: 29, width: 1000, height: 0.6)
+        bottomLayerEmail.backgroundColor = UIColor.white.cgColor
+        emailTextField.layer.addSublayer(bottomLayerEmail)
+        
+        usernameTextField.backgroundColor = UIColor.clear
+        usernameTextField.tintColor = UIColor.white
+        usernameTextField.textColor = UIColor.white
+        usernameTextField.attributedPlaceholder = NSAttributedString(string: usernameTextField.placeholder!, attributes: [NSAttributedString.Key.foregroundColor: UIColor(white: 1.0, alpha: 0.6)])
+        let bottomLayerUser = CALayer()
+        bottomLayerUser.frame = CGRect(x: 0, y: 29, width: 1000, height: 0.6)
+        bottomLayerUser.backgroundColor = UIColor.white.cgColor
+        usernameTextField.layer.addSublayer(bottomLayerUser)
+        
+        passTextField.backgroundColor = UIColor.clear
+        passTextField.tintColor = UIColor.white
+        passTextField.textColor = UIColor.white
+        passTextField.attributedPlaceholder = NSAttributedString(string: passTextField.placeholder!, attributes: [NSAttributedString.Key.foregroundColor: UIColor(white: 1.0, alpha: 0.6)])
+        let bottomLayerPass = CALayer()
+        bottomLayerPass.frame = CGRect(x: 0, y: 29, width: 1000, height: 0.6)
+        bottomLayerPass.backgroundColor = UIColor.white.cgColor
+        passTextField.layer.addSublayer(bottomLayerPass)
     }
     
 
+    @IBAction func signUpBtn_TouchUpInside(_ sender: Any) {
+        Auth.auth().createUser(withEmail: "user1@gmail.com", password: "123456") { (authResult, error) in
+            guard let user = authResult?.user else { return }
+        }
+    }
     /*
     // MARK: - Navigation
 
